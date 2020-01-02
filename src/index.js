1b41d1b09a96b9b91e2e435bc3570680f4569b27
JimmyMcBride <mcbride967@gmail.com>
2020-01-02 13:14:41 -0600

    enabled firebase auth and created login/register actions

--- src/index.js
+++ src/index.js
@@ -12,20 +12,38 @@ import * as serviceWorker from "./serviceWorker";
 // Set up Redux/Router
 import { applyMiddleware, createStore } from "redux";
 import { Provider } from "react-redux";
+import { ReactReduxFirebaseProvider } from "react-redux-firebase";
+
 import { BrowserRouter as Router } from "react-router-dom";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
+import firebase from "./config/firebase";
 
 // Import reducer/index.js as root reducer, it's where we're combining all our reducer files
 import rootReducer from "./store/reducers";
 
 const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
+const reactReduxFirebaseConfig = {
+  userProfile: "users",
+  useFirestoreForProfile: true,
+  attachAuthIsReady: true
+};
+
+const reactReduxFirebaseProps = {
+  firebase,
+  config: reactReduxFirebaseConfig,
+  dispatch: store.dispatch
+  // createFirestoreInstance // <- needed if using firestore
+};
+
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
+    <ReactReduxFirebaseProvider {...reactReduxFirebaseProps}>
+      <Router>
+        <App />
+      </Router>
+    </ReactReduxFirebaseProvider>
   </Provider>,
   document.getElementById("root")
 );
