77b697225bf4a82d14b910bc8ad418cc4c4e8bc3
Jimmy McBride <46766874+JimmyMcBride@users.noreply.github.com>
2020-01-02 14:01:43 -0600

    added projectBugs to combine reducers

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,9 +1,9 @@
 import { combineReducers } from "redux";
 import { auth } from "./auth";
-import { counter } from "./counter";
+import { projectBugs } from "./projectBugs";
 
 // Using combine reducers to break up reducers into different files
 export default combineReducers({
   auth,
-  counter
+  projectBugs
 });
