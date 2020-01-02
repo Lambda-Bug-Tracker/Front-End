12f5668a2d6a94f20635cdd0e1d01d3d4d308cf7
Robert Gordon <Komeuppance@gmail.com>
2020-01-02 10:54:24 -0800

    Used CSS for spinning animation

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,11 +1,35 @@
 import React from 'react';
 
+import styled from 'styled-components'
+
 import { Wrapper } from 'bushido-strap';
+
+const Container = styled.div`
+  img{
+    width: 200px;
+  }
+  .spinner {
+    animation: logo-spin infinite 5s linear;
+  }
+
+  @keyframes logo-spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+`
 
 export default function Loading() {
   return (
+    <Container>
     <Wrapper justify="center">
       <h2>Loading...</h2>
+      <img className='spinner' src='../../img/titled-bug.png' />
     </Wrapper>
+    </Container>
   );
 }
