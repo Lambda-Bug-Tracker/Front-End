e6ac0ea629d932e87bfdf60ca65ddbbf4a4486de
cladams0203 <cladams0203@gmail.com>
2020-01-02 17:27:59 -0500

    added basic card styles and functionality

--- src/components/BugBoard/BugCard.jsx
+++ src/components/BugBoard/BugCard.jsx
@@ -3,3 +3,33 @@
 /* admin edit and delete bug. (does not partain toa dditional notes*/
 
 /*on click -- brings up modal of notes*/
+
+import React from 'react'
+
+import { useDrag } from 'react-dnd'
+
+import { Card } from 'bushido-strap'
+import butterfly from '../../images/butterfly.png'
+
+
+export function BugCard(props) {
+    const [{isDragging}, drag] = useDrag({
+        item: {type: 'CARD', item: props.item},
+        collect: monitor => ({
+            isDragging: !!monitor.isDragging()
+        })
+    })
+    return(
+        <Card ref={drag} style={{opacity: isDragging ? 0.5 : 1}} width='90%' align='flex-start'  >
+            <div >
+            <img src={butterfly}  style={{width: '20%'}}/> 
+            <span> {props.name} Bug Name </span>
+            </div>
+        
+           <p> {props.description} Bug Description </p> 
+           <h5>!</h5>
+        </Card>
+    )
+}
+
+
