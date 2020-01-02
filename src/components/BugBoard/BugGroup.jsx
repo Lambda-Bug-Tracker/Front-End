18f5b9180dc9b2892d5abd838bfe99125f2461fd
Joscelyn <JoscelynRose@gmail.com>
2020-01-02 15:57:12 -0500

    set up mobile bug site

--- src/components/BugBoard/BugGroup.jsx
+++ src/components/BugBoard/BugGroup.jsx
@@ -1,21 +1,32 @@
 import React from "react";
+import { Wrapper, FlexBox } from "bushido-strap";
+
+import "./BugGroup.styles.scss";
 
 const BugGroup = () => {
   return (
-    <div className="bug-group-container">
-      <div className="bug-group">
-        <h4>Bugs To Squash</h4>
+    <Wrapper>
+      <div className="bug-group-container">
+        <FlexBox className="mobile-container" justify-content="space-between">
+          <div className="bug-group">
+            <h4 className="progressh4">Bugs To Squash</h4>
+            <div className="progress-column" />
+          </div>
+          <div className="bug-group">
+            <h4 className="progressh4">Currently Squashing</h4>
+            <div className="progress-column" />
+          </div>
+          <div className="bug-group">
+            <h4 className="progressh4">For Review</h4>
+            <div className="progress-column" />
+          </div>
+          <div className="bug-group">
+            <h4 className="progressh4">Squashed</h4>
+            <div className="progress-column" />
+          </div>
+        </FlexBox>
       </div>
-      <div className="bug-group">
-        <h4>Currently Squashing</h4>
-      </div>
-      <div className="bug-group">
-        <h4>For Review</h4>
-      </div>
-      <div className="bug-group">
-        <h4>Squashed</h4>
-      </div>
-    </div>
+    </Wrapper>
   );
 };
 
--- src/components/BugBoard/index.js
+++ src/components/BugBoard/index.js
@@ -2,23 +2,33 @@
 
 import React from "react";
 import BugGroup from "./BugGroup.jsx";
-import Logo from "../../images/logo.png";
-import HamburgerMenu from "../../images/hamburger-menu.png";
+import Logo from "../../images/tilt-left.png";
+import HamburgerMenu from "../../images/black-hamburger-menu.png";
 import "./styles.scss";
-import { Wrapper } from "bushido-strap";
+import { Wrapper, FlexBox } from "bushido-strap";
 
 const BugBoard = () => {
   return (
-    <Wrapper>
-      <header>
-        <div className="title">
+    <Wrapper className="bug-board-container">
+      <FlexBox className="top-container" justify-content="center" width="100%">
+        <FlexBox className="header-container" width="100%">
           <h1>Lambda Bug Tracker</h1>
-          <img alt="logo" src={Logo} />
-        </div>
-        <div className="hamburger-menu-1">
-          <img alt="hamburger-menu" src="{HamburgerMenu}" />
-        </div>
-      </header>
+          <FlexBox width="50px">
+            <img className="logo" alt="logo" src={Logo} />
+          </FlexBox>
+        </FlexBox>
+        <FlexBox
+          justify-content="center"
+          width="50px"
+          className="hamburger-menu-container"
+        >
+          <img
+            className="hamburger-menu"
+            alt="hamburger-menu"
+            src={HamburgerMenu}
+          />
+        </FlexBox>
+      </FlexBox>
       <main>
         <h2>Title</h2>
         <div className="bug-group-container">
