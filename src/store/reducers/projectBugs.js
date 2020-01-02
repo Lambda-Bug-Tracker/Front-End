aa040c7d5422f7cc8170db4c8fa36ebc9dad15a6
cladams0203 <cladams0203@gmail.com>
2020-01-02 14:50:27 -0500

    finished bug board reducer

--- src/store/reducers/projectBugs.js
+++ src/store/reducers/projectBugs.js
@@ -12,7 +12,58 @@ export const projectBugs = (state = initialState, action) => {
         case UPDATE_BUGSTOSQUASH :
             return {
                 ...state,
-                
+                bugsToSquash: [...state.bugsToSquash, action.payload.item],
+                squashing: state.squashing.filter(item => {
+                    return item.id !== action.payload.item.id
+                }),
+                review: state.review.filter(item => {
+                    return item.id !== action.payload.item.id
+                }),
+                squashed: state.squashed.filter(item => {
+                    return item.id !== action.payload.item.id
+                })
+            }
+        case UPDATE_SQUASHING :
+            return {
+                ...state,
+                squashing: [...state.squashing, action.payload.item],
+                bugsToSquash: state.bugsToSquash.filter(item => {
+                    return item.id !== action.payload.item.id
+                }),
+                review: state.review.filter(item => {
+                    return item.id !== action.payload.item.id
+                }),
+                squashed: state.squashed.filter(item => {
+                    return item.id !== action.payload.item.id
+                })
+            }
+        case UPDATE_REVIEW :
+            return {
+                ...state,
+                review: [...state.review, action.payload.item],
+                squashing: state.squashing.filter(item => {
+                    return item.id !== action.payload.item.id
+                }),
+                bugsToSquash: state.bugsToSquash.filter(item => {
+                    return item.id !== action.payload.item.id
+                }),
+                squashed: state.squashed.filter(item => {
+                    return item.id !== action.payload.item.id
+                })
+            }
+        case UPDATE_SQUASHED :
+            return {
+                ...state,
+                squashed: [...state.squashed, action.payload.item],
+                squashing: state.squashing.filter(item => {
+                    return item.id !== action.payload.item.id
+                }),
+                review: state.review.filter(item => {
+                    return item.id !== action.payload.item.id
+                }),
+                bugsToSquash: state.bugsToSquash.filter(item => {
+                    return item.id !== action.payload.item.id
+                })
             }
         default:
             return state
