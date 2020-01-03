79a7e0de3fd0ef66bf05fabd445371cf69a35cfa
Robert Gordon <Komeuppance@gmail.com>
2020-01-02 23:55:09 -0800

    BugBoard styling

--- src/components/BugBoard/index.js
+++ src/components/BugBoard/index.js
@@ -25,16 +25,10 @@ const BugBoard = () => {
         <UserBar />
       </FlexBox>
       <div className="right-container">
-        <FlexBox
-          className="top-container"
-          justify-content="center"
-          width="100%"
-        >
+        <FlexBox className="top-container" justify-content="center" width="100%">
           <FlexBox className="header-container" width="100%">
             <h1>Lambda Bug Tracker </h1>
-            <FlexBox width="50px">
               <img className="logo" alt="logo" src={Logo} />
-            </FlexBox>
           </FlexBox>
           <HamburgerMenu />
         </FlexBox>
