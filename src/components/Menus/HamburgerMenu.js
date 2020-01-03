b87fb0d2c611174a68174ed62f06c66688ac49f8
Joscelyn <JoscelynRose@gmail.com>
2020-01-03 09:17:45 -0500

    fixed up hamburger menu animation

--- src/components/Menus/HamburgerMenu.js
+++ src/components/Menus/HamburgerMenu.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import Burger from "@animated-burgers/burger-arrow";
+import BurgerSqueeze from "@animated-burgers/burger-squeeze";
 import "./menu-styles.styles.scss";
-
-// tryign to use this library: https://march08.github.io/animated-burgers/
 
 import BurgerImage from "../../images/black-hamburger-menu.png";
 import { Wrapper, FlexBox } from "bushido-strap";
 import "@animated-burgers/burger-arrow/dist/styles.css";
+import "@animated-burgers/burger-squeeze/dist/styles.css";
 import { Link } from "react-router-dom";
 // trying to use this library: https://march08.github.io/animated-burgers/
 
@@ -14,22 +14,18 @@ const HamburgerMenu = () => {
   const [openStatus, setOpenStatus] = useState(false);
   const handleClick = () => {
     setOpenStatus(!openStatus);
+    console.log(openStatus, "openStatus");
     //closeNav()
   };
   return (
-    <FlexBox className="hamburger-container">
+    <FlexBox onClick={() => handleClick()} className="hamburger-container">
       <FlexBox className="hamburger-picture-container">
-        <img
-          alt="hamburger menu"
-          src={BurgerImage}
-          className="open"
-          width="50"
-          onClick={() => handleClick()}
-        />
+        <div className="burger-squeeze-container">
+          <BurgerSqueeze isOpen={openStatus} alt="hamburger menu" />
+        </div>
       </FlexBox>
 
       <div id="mySidenav" className={openStatus ? "sidenav" : "sidenavclose"}>
-        <Burger direction="right" onClick={() => handleClick()} isOpen={true} />
         {openStatus ? (
           <div>
             <Link to="/">Home</Link>
