0eb027bab474b2a5594457a6cf27115e8c2239f4
Robert Gordon <Komeuppance@gmail.com>
2020-01-02 17:03:27 -0800

    Imported styling from other components

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -18,8 +18,25 @@ export default function Dashboard() {
     <Wrapper>
       <div className='main'>
         <div className='dashboard'>
-          <h1>Hello, world!</h1>
-          <Button onClick={handleSignOut}>Sign Out</Button>
+          <div className='top-row'>
+            <Button onClick={handleSignOut}>Sign Out</Button></div>
+            <div className='main-container'>
+              <h2>Welcome First_Name Last_Name!</h2>
+              <div className="bug-group-container">
+                <div className="bug-group">
+                  <h4 className="progressh4">Projects I belong to</h4>
+                    <div className="progress-column">
+                      List of projects
+                    </div>
+                  </div>
+                <div className="bug-group">
+                  <h4 className="progressh4">Projects I am admin of</h4>
+                    <div className="progress-column">
+                      List of projects
+                    </div>
+                  </div>
+              </div>
+            </div>
         </div>
       <svg viewBox="0 0 100 100" preserveAspectRatio="none">
           <polygon points="0 0,100 0,100 100,0 100" />
