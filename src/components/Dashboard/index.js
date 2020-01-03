fb94c7f6f757f53b4f339c089f362c6998a7a2ed
Robert Gordon <Komeuppance@gmail.com>
2020-01-02 16:29:01 -0800

    Background svg added

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -6,6 +6,8 @@ import { useDispatch } from 'react-redux';
 
 import { signOut } from '../../store/actions/auth';
 
+import "./styles.scss";
+
 export default function Dashboard() {
   const dispatch = useDispatch();
 
@@ -14,9 +16,13 @@ export default function Dashboard() {
   }
   return (
     <Wrapper>
+      <div className='main'>
       <h1>Hello, world!</h1>
       <Button onClick={handleSignOut}>Sign Out</Button>
-      
+      <svg viewBox="0 0 100 100" preserveAspectRatio="none">
+          <polygon points="0 0,100 0,100 100,0 100" />
+        </svg>
+      </div>
     </Wrapper>
   );
 }
