0e58f55550115a75a4e8fff15bd92297bad9f036
cladams0203 <cladams0203@gmail.com>
2020-01-02 20:52:53 -0500

    started bug modal

--- src/components/BugBoard/BugModal.js
+++ src/components/BugBoard/BugModal.js
@@ -1,5 +1,21 @@
 /* All users can add a bug
-
 admins can edit and delete bug
-
 populates with data*/
+import React, { useState } from 'react'
+
+
+export function BugModal() {
+    const [form, setForm] = useState({
+        name:'',
+        description: '',
+        notes: [],
+        priority: '',
+        bugType: ''
+
+    })
+    return(
+        <div>
+
+        </div>
+    )
+}
--- src/components/Menus/HamburgerMenu.js
+++ src/components/Menus/HamburgerMenu.js
@@ -2,8 +2,9 @@ import React from "react";
 import { FlexBox, Wrapper } from "bushido-strap";
 import Burger from "@animated-burgers/burger-arrow";
 import "./menu-styles.styles.scss";
+import '@animated-burgers/burger-arrow/dist/styles.css'
 
-import "@animated-burgers/burger-arrow/dist/styles.css";
+
 // tryign to use this library: https://march08.github.io/animated-burgers/
 const HamburgerMenu = () => {
   return (
