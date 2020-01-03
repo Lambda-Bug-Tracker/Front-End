f5ee5408a9cc5862fe438e0ce0fbde7d847a11c1
cladams0203 <cladams0203@gmail.com>
2020-01-02 19:07:46 -0500

    added drop functionality to column1 and column2

--- src/components/BugBoard/Columns/Column2.js
+++ src/components/BugBoard/Columns/Column2.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { UPDATE_SQUASHING } from '../../../store/actions/projectBugs'
+import { useDrop } from 'react-dnd'
+import { BugCard } from '../BugCard'
 
 const Column2 = () => {
+    const data = useSelector(state => state.projectBugs)
+    const dispatch = useDispatch()
+    const [{isOver}, drop] = useDrop({
+        accept: 'CARD',
+        drop(item) {
+            dispatch({type:UPDATE_SQUASHING, payload: item})
+        },
+        collect: monitor => ({
+            isOver: !!monitor.isOver()
+        })
+    })
     return (
     <div className="bug-group">
         <h4 className="progressh4">Currently Squashing</h4>
-        <div className="progress-column" />
+        <div className="progress-column" ref={drop}>
+            {data.squashing.map((item, index) => {
+                return <BugCard item={item} key={index} />
+            })}
+        </div>
     </div>
 )
 }
