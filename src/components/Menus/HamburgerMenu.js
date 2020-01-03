6150195917b6754b3d6a2276df6781a54435b1b6
Joscelyn <JoscelynRose@gmail.com>
2020-01-02 19:52:01 -0500

    added reference for library

--- src/components/Menus/HamburgerMenu.js
+++ src/components/Menus/HamburgerMenu.js
@@ -4,7 +4,7 @@ import Burger from "@animated-burgers/burger-arrow";
 import "./menu-styles.styles.scss";
 
 import "@animated-burgers/burger-arrow/dist/styles.css";
-
+// tryign to use this library: https://march08.github.io/animated-burgers/
 const HamburgerMenu = () => {
   return (
     <div className="background-black">
