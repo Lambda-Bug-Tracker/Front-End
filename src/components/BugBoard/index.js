b899044028d5f470416ab08e6c49316f798f17b3
Robert Gordon <Komeuppance@gmail.com>
2020-01-02 23:32:52 -0800

    Added AddNewBug.js

--- src/components/BugBoard/index.js
+++ src/components/BugBoard/index.js
@@ -1,7 +1,8 @@
 /* holds -- state of bugs of that project and drag and drop. renders: state columns bug cards*/
 
-import React from "react";
+import React, {useState} from "react";
 import BugGroup from "./BugGroup.jsx";
+import AddNewBug from "./AddNewBug";
 import Logo from "../../images/tilt-left.png";
 import HamburgerMenu from "../Menus/HamburgerMenu.js";
 import "./styles.scss";
@@ -10,6 +11,14 @@ import { Wrapper, FlexBox } from "bushido-strap";
 import UserBar from "../UserBar";
 
 const BugBoard = () => {
+
+  const [addingNewBug, setAddingNewBug ] = useState(false)
+
+  const handleAddNewBug = e => {
+    e.preventDefault();
+    setAddingNewBug(true)
+  }
+
   return (
     <Wrapper className="bug-board-container" width="100%">
       <FlexBox className="sidebar-container">
@@ -30,11 +39,17 @@ const BugBoard = () => {
           <HamburgerMenu />
         </FlexBox>
         <main>
-          <h2>Title</h2>
-          <button>Add New Bug</button>
+          {addingNewBug ? <AddNewBug setAddingNewBug={setAddingNewBug} /> : 
+          <>
+          <div className="title-container">
+            <h2>Title</h2>
+            <button onClick={handleAddNewBug}>Add New Bug</button>
+          </div>
           <div className="bug-group-container">
             <BugGroup />
           </div>
+          </>
+          }
         </main>
       </div>
     </Wrapper>
