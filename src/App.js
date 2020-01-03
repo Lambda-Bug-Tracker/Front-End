843237f17d54e00b6e605a48b48eabc8e45c8dec
Brianna Keune <briannakeune@gmail.com>
2020-01-03 10:26:37 -0600

    updated private routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 // Set up all routes in App
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 //drag and drop library provider
 import { DndProvider } from "react-dnd";
@@ -18,20 +19,24 @@ import Register from "./components/Auth/Register";
 import Login from "./components/Auth/Login";
 import Dashboard from "./components/Dashboard";
 import { BugModal } from "./components/BugBoard/BugModal";
+import LandingPage from "./components/LandingPage/index.js";
 
 const App = () => {
-  
+  const firebase = useSelector(state => state.firebase)
   return (
     <DndProvider backend={Backend}>
       <AppWrapper>
-        <PrivateRoute path="/" exact component={Dashboard} />
-        <Route path="/project/:id" component={BugBoard} />
+
+        <Switch>
+          <PrivateRoute path="/project/:id" component={BugBoard} />
+          <PrivateRoute path="/bug-modal/:id" component={BugModal} />
+
+        </Switch>
+          <Route exact path="/" component={firebase.auth.isEmpty ? LandingPage : Dashboard} />
+
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
 
-        <Route path="/dashboard" component={Dashboard} />
-
-        <Route path="/bug-modal/:id" component={BugModal} />
       </AppWrapper>
     </DndProvider>
   );
