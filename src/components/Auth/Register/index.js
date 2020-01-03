21e7347a20174b39bf68069aeb89459b510ab259
Robert Gordon <Komeuppance@gmail.com>
2020-01-02 19:01:32 -0800

    Register styling complete

--- src/components/Auth/Register/index.js
+++ src/components/Auth/Register/index.js
@@ -3,36 +3,16 @@ import { useForm } from "react-hook-form";
 import * as yup from "yup";
 
 import Loading from "../../Loading/";
+import lambdaBanner from '../../../assets/img/lambda-banner.png';
 
 import { Form, Input, Button } from "bushido-strap";
 
-import styled from "styled-components";
+import "./styles.scss";
 
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router";
 
 import { authenticate } from "../../../store/actions/auth";
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin-top: 150px;
-  .title {
-    margin: 10px;
-  }
-  .error {
-    text-align: center;
-    margin: 5px;
-    color: red;
-  }
-
-  p {
-    color: red;
-  }
-`
-
 
 const schema = yup.object().shape({
   first_name: yup.string().required('First name required!'),
@@ -77,24 +57,31 @@ export default function Register() {
   };
 
   return (
-    <Container>
-      <div className="title">Register for Lambda Bugtracker</div>
+    <div className="main">
+      <div className="dashboard">
+        <div className='top-row'>
+            <img src={lambdaBanner} alt="Lambda School Logo" />
+        </div>
+        <div className="title">Register for Lambda Bugtracker</div>
+          { isLoading ? <Loading /> : 
+            <Form onSubmit={handleSubmit(onSubmit)}>
+              <Input type="text" name='first_name' value={data.first_name} onChange={handleChange} placeholder="First Name" ref={register} />
+              {errors.first_name && <p>{errors.first_name.message}</p>}
+              <Input type="text" name='last_name' value={data.last_name} onChange={handleChange} placeholder="Last Name" ref={register} />
+              {errors.last_name && <p>{errors.last_name.message}</p>}
+              <Input type="text" name='email' value={data.email} onChange={handleChange} placeholder="Email" ref={register} />
+              {errors.email && <p>{errors.email.message}</p>}
+              <Input type="password" name='password' value={data.password} onChange={handleChange} placeholder="Password" ref={register} />
+              {errors.password && <p>{errors.password.message}</p>}
 
-      { isLoading ? <Loading /> : 
-        <Form onSubmit={handleSubmit(onSubmit)}>
-          <Input type="text" name='first_name' value={data.first_name} onChange={handleChange} placeholder="First Name" ref={register} />
-          {errors.first_name && <p>{errors.first_name.message}</p>}
-          <Input type="text" name='last_name' value={data.last_name} onChange={handleChange} placeholder="Last Name" ref={register} />
-          {errors.last_name && <p>{errors.last_name.message}</p>}
-          <Input type="text" name='email' value={data.email} onChange={handleChange} placeholder="Email" ref={register} />
-          {errors.email && <p>{errors.email.message}</p>}
-          <Input type="password" name='password' value={data.password} onChange={handleChange} placeholder="Password" ref={register} />
-          {errors.password && <p>{errors.password.message}</p>}
-
-          <Button type="submit">Register</Button>
-          {error && <div className="error">{error}</div>}
-        </Form>
-      }
-    </Container>
+              <Button type="submit">Register</Button>
+              {error && <div className="error">{error}</div>}
+            </Form>
+          }
+        </div>
+        <svg viewBox="0 0 100 100" preserveAspectRatio="none">
+          <polygon points="0 0,100 0,100 100,0 100" />
+        </svg>
+    </div>
   );
 }
