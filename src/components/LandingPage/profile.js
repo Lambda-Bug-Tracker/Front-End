3f0281154c4013e32520b47e4571926d08660319
Aaron Shawn Soler <aaronshawnsoler@gmail.com>
2020-01-02 20:42:36 -0500

    marketing page done

---
+++ src/components/LandingPage/profile.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function ProfilePic(props) {
+    return(
+        <div className='profile-pic'>
+            <img src={props.data.image} alt={props.data.name} />
+            <div className='overlay'>
+                <h4>{props.data.name}</h4>
+                <p>{props.data.snip}</p>
+                <div className='social-icons'>
+                    <ion-icon className='logo-github' name="logo-github"></ion-icon>
+                    <ion-icon className='logo-linkedin' name="logo-linkedin"></ion-icon>
+                    <ion-icon className='logo-twitter' name="logo-twitter"></ion-icon>
+                </div>
+            </div>
+        </div>
+    )
+}
